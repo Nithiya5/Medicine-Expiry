7effaeee8fa25e7777d507a92c453cacfaf011fa
Nithiya5 <nithiyaprabha.r2022@sece.ac.in>
2025-03-31 16:15:20 +0530

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb+srv://nithiyaprabhar:nithiya2005@cluster0.a02jqzo.mongodb.net/MedicineExpiry", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
+
+  app.use("/api/user", userRoutes);
+
+
+
+
+const PORT = 7000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
