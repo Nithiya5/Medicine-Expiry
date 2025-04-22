e973eef7dbf1b69b9c6eb0826164d84ae9d10066
R Kanishkasree <kanishkasree804@gmail.com>
2025-04-22 10:36:53 +0530

    admin login

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { login } = require('../controller/loginController');  
 
+
 router.post('/login', login);
 
 module.exports = router;
